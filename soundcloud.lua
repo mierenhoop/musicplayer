1ed9fecd427a2123d2de9e660a2d21284844afcb
mierenhoop <unconfigured@null.spigotmc.org>
2021-05-01 15:22:42 +0000

    Add archive implementation

--- soundcloud.lua
+++ soundcloud.lua
@@ -6,10 +6,11 @@ local url = require "url"
 local fs = require "fs"
 local dom = require "dom"
 
+local limit = 1000
 local apiurl = "https://api-v2.soundcloud.com"
 
-function randomid()
-   local ids = {"1ZblcwqkcM5jJpypOqMGosmsvOPj2yqW","5MlCU9alf35yL0Ub7owwSlLVcGLgiFIB"} -- TODO: Add more
+local function randomid()
+   local ids = {"1ZblcwqkcM5jJpypOqMGosmsvOPj2yqW","5MlCU9alf35yL0Ub7owwSlLVcGLgiFIB","Jso9j707fOmQVz48JLun3FOSX7ir5SPH"} -- TODO: Add more
    return ids[math.random(#ids)]
 end
 
@@ -23,14 +24,70 @@ local function getjson(path, qs, cb)
       res:on("end", function()
          if res.statusCode == 200 then
             cb(json.parse(data))
-         else p(res)
+         else p(res.statusCode)
          end
       end)
    end)
    req:on("error", function(err) p("Got error", err) end)
 end
 
+local function getusertracks(userid, cb)
+   getjson(apiurl .. "/users/" .. tostring(userid) .. "/tracks", {limit = limit}, function(o)
+      cb(o.collection)
+   end)
+end
 
+-- Hack: get userid by search itself
+local function getuserid(userlink, cb)
+   getjson(apiurl .. "/search/users", { q = userlink, limit = 10 }, function(o)
+      for _, item in ipairs(o.collection) do
+         if item.permalink == userlink then
+            cb(item.id)
+            return
+         end
+      end
+      error "Couldn't find user"
+   end)
+end
+
+local function gettrackmp3(trackjson, cb)
+   local stream = trackjson.media.transcodings[2]
+   assert(stream.format.protocol == "progressive")
+   getjson(stream.url, {}, function(o)
+      cb(o.url)
+   end)
+end
+
+local archivepath = "/tmp/archive"
+
+local function archiveuser(link)
+   local permalink = link:sub(24)
+   fs.mkdir(archivepath, function() end)
+   local userpath = archivepath .. "/" .. permalink
+   fs.mkdir(userpath, function() end)
+   print("Searching user: " .. permalink)
+   getuserid(permalink, function(id)
+      print("Found user with id: " .. id)
+      getusertracks(id, function(tracks)
+         print("Found " .. #tracks .. " tracks")
+         for _, track in ipairs(tracks) do
+            gettrackmp3(track, function(link)
+               print("Found track download link for " .. track.title)
+               https.get(link, function(res)
+                  local trackpath = userpath .. "/" .. track.permalink .. ".mp3"
+                  stream = fs.createWriteStream(trackpath)
+                  res:pipe(stream)
+                  print("Downloaded track to: " .. trackpath)
+               end)
+            end)
+         end
+      end)
+   end)
+end
+
+if args[2] then archiveuser(args[2]) end
+
+--[[
 http.createServer(function(req, res)
    local uri = url.parse(req.url)
    if uri.pathname == "/" then
@@ -117,3 +174,4 @@ http.createServer(function(req, res)
 end):listen(8080)
 
 p("Running server at http://127.0.0.1:8080")
+]]
